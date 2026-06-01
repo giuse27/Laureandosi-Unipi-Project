61f9ecf2753b5ef74ed744096ecee223e4625517
Giuseppe Vaglica <giuseppe.vaglica3@gmail.com>
2026-06-01 22:03:06 +0200

    Semplificato il metodo di istanza di FormulaMedia

--- src/Config/FormulaMedia.php
+++ src/Config/FormulaMedia.php
@@ -60,7 +60,18 @@ class FormulaMedia {
 
 
 
-    public function __construct(array $dati) {
+    public function __construct(string $path) {
+
+        if (!file_exists($path)) {
+            throw new \RuntimeException("File di configurazione non trovato in: $path");
+        }
+
+        $json = file_get_contents($path);
+        $dati = json_decode($json, true);
+
+        if (json_last_error() !== JSON_ERROR_NONE) {
+            throw new \RuntimeException("JSON non valido: " . json_last_error_msg());
+        }
 
         $this->cdl              = (string)  ($dati['cdl']               ?? $this->cdlShort  );
         $this->cdlAlt           = (string)  ($dati['cdlAlt']            ?? ''               );
