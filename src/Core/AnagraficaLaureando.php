849f51044710fcec5cba7868519237a6888b7151
Giuseppe Vaglica <giuseppe.vaglica3@gmail.com>
2026-06-01 19:07:19 +0200

    Implementazione della classe AnagraficaLaureando

--- src/Core/AnagraficaLaureando.php
+++ src/Core/AnagraficaLaureando.php
@@ -1,1 +1,27 @@
 <?php
+
+namespace Laureandosi\Core;
+
+require_once dirname(__DIR__, 2) . '/vendor/autoload.php';
+
+use Laureandosi\API\GestioneCarrieraStudente;
+
+/**
+ * Dati anagrafici di uno studente laureando.
+ * Popolata da GestioneCarrieraStudente::restituisciAnagraficaStudente().
+ */
+class AnagraficaLaureando
+{
+    public readonly string $nome;
+    public readonly string $cognome;
+    public readonly string $email;
+
+    public function __construct(string $matricola)
+    {
+        $studente = GestioneCarrieraStudente::RestituisciAnagraficaStudente($matricola);
+        $this->nome = $studente['nome'];
+        $this->cognome = $studente['cognome'];
+        $this->email = $studente['EmailAteneo'];
+    }
+
+}
