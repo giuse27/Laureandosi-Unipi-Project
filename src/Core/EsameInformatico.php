fba86908c61e09a5a428dd8e58355aaf8feb6b4d
Giuseppe Vaglica <giuseppe.vaglica3@gmail.com>
2026-06-01 23:19:53 +0200

    Creazione della classe EsameInformatico

--- src/Core/EsameInformatico.php
+++ src/Core/EsameInformatico.php
@@ -1,1 +1,19 @@
 <?php
+
+namespace Laureandosi\Core;
+
+require_once dirname(__DIR__, 2) . '/vendor/autoload.php';
+
+use Laureandosi\Config\FileConfigurazione;
+use Laureandosi\Core\Esame;
+
+class EsameInformatico extends Esame
+{
+    public readonly bool $informatico;
+
+    public function __construct(array $esameJSON, string $cdl, string $mat)
+    {
+        $this->informatico = true;
+        parent::__construct($esameJSON, $cdl, $mat);
+    }
+}
