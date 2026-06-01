7590b550ffc0cb1c47b85522adceeebc777dbb60
Giuseppe Vaglica <giuseppe.vaglica3@gmail.com>
2026-06-01 22:48:58 +0200

    Metodi isValid e faMedia per la classe FiltroEsame

--- src/Config/FiltroEsami.php
+++ src/Config/FiltroEsami.php
@@ -2,6 +2,8 @@
 
 namespace Laureandosi\Config;
 
+use Laureandosi\Core\Esame;
+
 class FiltroEsami
 {
     private array $datiJSON;
@@ -20,5 +22,37 @@ class FiltroEsami
         }
 
         $this->datiJSON = $dati;
+    }
+
+    public function isValid(Esame $esame, string $cdl, string $mat) : bool
+    {
+        $nomeEsame = $esame->getNomeEsame();
+
+        // Verifico se l'esame è presente nei filtri globali
+        if (in_array($nomeEsame, $this->datiJSON['global']['NoCdl'] ?? [], true)) return false;
+
+        // Verifico se l'esame è presente nella lista dei filtri del CdL per gli esami non validi
+        if (in_array($nomeEsame, $this->datiJSON[$cdl]['NoCdl'] ?? [], true)) return false;
+
+        // Verifico se l'esame non può essere sostenuto dallo studente specifico
+        if (in_array($nomeEsame, $this->datiJSON['specific'][$mat]['NoCdl'] ?? [], true)) return false;
+
+        return true;
+    }
+
+    public function faMedia(Esame $esame, string $cdl, string $mat): bool
+    {
+        $nomeEsame = $esame->getNomeEsame();
+
+        // Verifico se l'esame è presente nei filtri globali
+        if (in_array($nomeEsame, $this->datiJSON['global']['NoAvg'] ?? [], true)) return false;
+
+        // Verifico se l'esame è presente nella lista dei filtri del CdL per gli esami che non fanno media
+        if (in_array($nomeEsame, $this->datiJSON[$cdl]['NoAvg'] ?? [], true)) return false;
+
+        // Verifico se l'esame non fa media per lo studente specifico
+        if (in_array($nomeEsame, $this->datiJSON['specific'][$mat]['NoAvg'] ?? [], true)) return false;
+
+        return true;
     }
 }
