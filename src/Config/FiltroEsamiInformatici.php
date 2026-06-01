0103b44e52a7266acbaf291bcb332ef1363e16cd
Giuseppe Vaglica <giuseppe.vaglica3@gmail.com>
2026-06-01 22:02:04 +0200

    Costruttore per la classe FiltroEsamiInformatici

--- src/Config/FiltroEsamiInformatici.php
+++ src/Config/FiltroEsamiInformatici.php
@@ -1,1 +1,28 @@
 <?php
+
+namespace Laureandosi\Config;
+
+require_once dirname(__DIR__, 2) . '/vendor/autoload.php';
+
+use Laureandosi\Config\FiltroEsami;
+
+class FiltroEsamiInformatici
+{
+    private array $datiJSON;
+
+    public function __construct(string $path)
+    {
+        if (!file_exists($path)) {
+            throw new \RuntimeException("File di configurazione non trovato in: $path");
+        }
+
+        $json = file_get_contents($path);
+        $dati = json_decode($json, true);
+
+        if (json_last_error() !== JSON_ERROR_NONE) {
+            throw new \RuntimeException("JSON non valido: " . json_last_error_msg());
+        }
+
+        $this->datiJSON = $dati;
+    }
+}
