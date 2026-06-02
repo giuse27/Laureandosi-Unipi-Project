fe11dbc63fbd45e66c44a23dc5607acf4c52d936
Giuseppe Vaglica <giuseppe.vaglica3@gmail.com>
2026-06-02 18:58:35 +0200

    Calcolo della media e dei cfu del laureando in CarrieraLaureando

--- src/Core/CarrieraLaureando.php
+++ src/Core/CarrieraLaureando.php
@@ -6,12 +6,14 @@ require_once dirname(__DIR__, 2) . '/vendor/autoload.php';
 
 use Laureandosi\API\GestioneCarrieraStudente;
 use Laureandosi\Config\FileConfigurazione;
+use Laureandosi\Core\Esame;
 
 class CarrieraLaureando
 {
     private string $matricola;
     private string $cdl;
     private string $dataLaurea;
+    /** @var Esame[] */
     private array $esamiValidi = [];
     private int $cfuTotali = 0;
     private int $cfuMedia = 0;
@@ -31,13 +33,60 @@ class CarrieraLaureando
         foreach ($carriera['Esami']['Esame'] as $esame) {
             if ($esame['SOVRAN_FLG'] === 0 && $config->isValid($esame['DES'], $cdl, $mat)) {
                 $e = new Esame($esame, $cdl, $mat);
-                $this->EsamiValidi[] = $e;
+                $this->esamiValidi[] = $e;
             }
         }
 
         // Calcola subito CFU e Media dopo aver recuperato gli esami e salvo i risultati nelle proprietà della classe
-        // $this->ContaCFU();
-        // $this->CalcolaMediaPesata();
+        $this->cfuTotali = $this->getTotaleCfu();
+        $this->cfuMedia = $this->getCfuMedia();
+        $this->media = $this->getMedia();
+    }
+
+    public function getTotaleCfu(): int
+    {
+        $cfu_totali = 0;
+
+        /** @var Esame $esame */
+        foreach ($this->esamiValidi as $esame) {
+            $cfu_totali += $esame->getCfu();
+        }
+
+        return $cfu_totali;
+    }
+
+    public function getCfuMedia(): int
+    {
+        $cfu_media = 0;
+
+        /** @var Esame $esame */
+        foreach ($this->esamiValidi as $esame) {
+            if($esame->isFaMedia()) {
+                $cfu_media += $esame->getCfu();
+            }
+        }
+
+        return $cfu_media;
+    }
+
+    public function getMedia(): float
+    {
+        $media_pesata = 0.0;
+        $somma_pesata = 0;
+
+        // per calcolare la media pesata sommo voto*cfu e divido per cfuMedia
+        /** @var Esame $esame */
+        foreach ($this->esamiValidi as $esame) {
+            if($esame->isFaMedia()) {
+                $somma_pesata += $esame->getVoto() * $esame->getCfu();
+            }
+        }
+
+        if ($this->cfuMedia > 0) {
+            $media_pesata = $somma_pesata / $this->cfuMedia;
+        }
+        
+        return $media_pesata;
     }
 
 }
